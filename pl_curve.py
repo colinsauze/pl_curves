a90718242580d1360417e3d33a43eb6c1ac78c67
WYVERN2742 <35181365+WYVERN2742@users.noreply.github.com>
2021-03-24 11:34:57 +0000

    Convert series to array due to pandas deprication

--- pl_curve.py
+++ pl_curve.py
@@ -63,7 +63,7 @@ def calculate_gini(data):
         return math.nan
 
     # Mean absolute difference
-    mad = np.abs(np.subtract.outer(data, data)).mean()
+    mad = np.abs(np.subtract.outer(np.array(data), np.array(data))).mean()
     # Relative mean absolute difference
     rmad = mad / np.mean(data)
     # Gini coefficient
